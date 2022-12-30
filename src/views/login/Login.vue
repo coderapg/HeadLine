1f38659c8ff7a7838296fb1d420a46b4b99b9d12
李永峰 <1184156787@qq.com>
2022-12-30 17:12:12 +0800

    登录页面布局

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,13 +1,69 @@
 <template>
-  <div class="login-container">登录</div>
+  <div class="login-container">
+    <!-- 顶部导航栏 -->
+    <van-nav-bar
+      title="登录"
+      @click-left="handleBack">
+      <template #left>
+        <van-icon name="cross" size="18" />
+      </template>
+    </van-nav-bar>
+    <van-cell-group>
+    <!-- /顶部导航栏 -->
+    <!-- 用户输入框 -->
+      <van-field
+        v-model="user.mobile"
+        icon-prefix="icon"
+        left-icon="shouji"
+        placeholder="请输入手机号" />
+      <van-field
+        v-model="user.code"
+        clearable
+        icon-prefix="icon"
+        left-icon="yanzhengma"
+        placeholder="请输入验证码">
+        <template #button>
+          <van-button size="mini" color="#3296fa" round>发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <!-- /用户输入框 -->
+    <van-button type="info" block color="#3296fa">信息按钮</van-button>
+  </div>
 </template>
 
 <script>
+import { handleBackMixin } from 'utils/mixin'
 
 export default {
-  name: 'Login'
+  name: 'Login',
+  mixins: [handleBackMixin],
+  data () {
+    return {
+      user: {
+        mobile: '13611111111',
+        code: '246810'
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+  .login-container {
+    /deep/ .van-nav-bar {
+      background-color: #3296fa;
+      .van-nav-bar__content {
+        height: 44px;
+        .van-nav-bar__left {
+          .van-icon {
+            color: #f2f2f2;
+          }
+        }
+        .van-nav-bar__title {
+          color: #fff;
+        }
+      }
+    }
+  }
 </style>
